0fb8e94842dc418b57cd5385cc91c3ca5830a74b
Dino Pacandi <dino.pacandi@gmail.com>
2024-12-14 11:24:40 +0100

    Config file

--- src/main.rs
+++ src/main.rs
@@ -3,61 +3,79 @@ mod parser;
 mod types;
 mod validation;
 
+use fifo::InventoryItem;
 use types::OutputLine;
 
-fn main() {
+use serde::Deserialize;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
-    let sheet_2023 = parser::parse_xlsx_file("balances.xlsx", "2023", 1).unwrap();
-    let mut result = validation::validate_sheet(&sheet_2023, Default::default(), "2023").unwrap();
+    // 0. Parse the config file
+    let toml_content = std::fs::read_to_string("Config.toml")?;
+    let config: Config = toml::from_str(&toml_content)?;
 
-    let sheet_2024 = parser::parse_xlsx_file("balances.xlsx", "2024", 14).unwrap();
-    result = validation::validate_sheet(&sheet_2024, result, "2024").unwrap();
+    // 1. Parse the XLSX files and validate the data.
+    let mut asset_state = Default::default();
+    let mut transactions = Vec::new();
 
-    println!("Final state: {:#?}", result);
-    println!("====================");
+    for entry in config.entries {
+        let sheet = parser::parse_xlsx_file(&entry.path, &entry.sheet, entry.start_row)?;
+        asset_state = validation::validate_sheet(&sheet, asset_state, &entry.sheet)?;
 
+        transactions.extend(sheet.into_iter());
+    }
+
+    // 2. Assign ascending ordinals to all transactions.
     let mut counter: u32 = 0;
-    let transactions: Vec<_> = sheet_2023
+    transactions = transactions
         .into_iter()
-        .chain(sheet_2024.into_iter())
         .map(|tx| {
             counter += 1;
             tx.new_with_ordinal(counter)
         })
         .collect();
 
+    // 3. Create the ledger & process the transactions in FIFO manner.
     let mut ledger = fifo::Ledger::new(transactions);
 
+    // 4. Generate the output CSV file.
     let lines = ledger
         .output_lines()
         .into_iter()
-        .map(|line| line.to_csv_line(";".to_string()))
+        .map(|line| line.to_csv_line(config.csv_delimiter.clone()))
         .collect::<Vec<_>>();
 
     // Write the output to a file.
     std::fs::write(
-        "output.csv",
+        config.output_path,
         format!(
             "{}\n{}",
-            OutputLine::csv_header(";".to_string()),
+            OutputLine::csv_header(config.csv_delimiter),
             lines.join("\n")
         ),
     )
     .unwrap();
 
-    // ledger.in_order()
-    //     .iter()
-    //     .for_each(|tx| {
-    //         println!("{}", tx.report());
-    //     });
+    Ok(())
+}
 
-    // println!("State after processing:");
-    // println!("{}", ledger.remaining_amount_report());
+#[derive(Debug, Deserialize)]
+struct Config {
+    /// Path to the output file.
+    output_path: String,
+    /// Separator to use in the output CSV file.
+    csv_delimiter: String,
+    /// List of entries to parse.
+    entries: Vec<FileEntry>,
+}
 
-    // ledger.ledger()[&crate::types::AssetType::EUR]
-    //     .iter()
-    //     .for_each(|tx| {
-    //         println!("{:?}", tx);
-    //     });
+#[derive(Debug, Deserialize)]
+struct FileEntry {
+    /// Path to the XLSX file.
+    path: String,
+    /// Name of the sheet to read from.
+    sheet: String,
+    /// Row number from which to start reading the data.
+    start_row: usize,
 }
